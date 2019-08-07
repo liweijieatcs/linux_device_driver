ac81efba822d632aff52bdd76048ff88e6ba3ba6
liweijie <ee.liweijie@gmail.com>
2019-08-07 07:43:10 -0700

    use alloc_chrdev_region() for alloc region
    when failed and rmmod moudle use unregister_chrdev_region()for remove
    the region.
    
    ismod the globalmem.ko
    you can see the region use the "cat /proc/device"
    when you rmmod globamem, the region is disappear

--- global_mem/globalmem.c
+++ global_mem/globalmem.c
@@ -1,13 +1,26 @@
 #include <linux/module.h>
+#include <linux/cdev.h>         /* for alloc_chrdev_region */
 
+dev_t dev_no;
 static int __init global_mem_init(void)
 {
+    int ret;
+    int major;
+    int minor;
+    ret = alloc_chrdev_region(&dev_no, 0, 1, "globalmem");
+    if (ret < 0) {
+        printk(KERN_INFO "fail to alloc chrdev region\n");
+    }
+    major = MAJOR(dev_no);
+    minor = MINOR(dev_no);
+    printk(KERN_INFO "chrdev major:%d, minor:%d\n", major, minor);
     printk(KERN_INFO "global_mem init success.\n");
     return 0;
 }
 
 static void __exit global_mem_exit(void)
 {
+    unregister_chrdev_region(dev_no, 1);
     printk(KERN_INFO "global_mem exit success.\n");
     return;
 }
