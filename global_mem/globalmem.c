1e2c01c3992291385b9c980391d466870fe39f05
liweijie <ee.liweijie@gmail.com>
2019-08-07 20:01:41 -0700

    add fops of device
    add write read operations for the device
    now we can manipulate the device via echo/cat
    
    first mknod /dev/global_mem c 243 0
    write the device: echo "Hello world" > /dev/global_mem
    read from the device : cat /dev/glbal_mem
    
    because the driver work in the kernel space,it's change
    date from usespace,we use the copy_from(to)_user

--- global_mem/globalmem.c
+++ global_mem/globalmem.c
@@ -1,6 +1,7 @@
 #include <linux/module.h>
 #include <linux/cdev.h>         /* for alloc_chrdev_region */
 #include <linux/slab.h>         /* for kzlloc */
+#include <linux/uaccess.h>      /* for copy_from(to)_user */
 
 #define GLOBAL_MEM_SIZE 4096
 
@@ -13,8 +14,75 @@ struct global_mem_dev {
 };
 struct global_mem_dev *global_mem_devp;
 
+
+int global_mem_open(struct inode *inode, struct file *filp)
+{
+    /* set the device pointer to the file private data
+     * then we can manipulate the device via private data
+     */
+    filp->private_data = global_mem_devp;
+    return 0;
+}
+
+int global_mem_release(struct inode *inode, struct file *filp)
+{
+    return 0;
+}
+
+static ssize_t global_mem_read(struct file *filp, char __user *buf, size_t size, loff_t *ppos)
+{
+    unsigned long p =  *ppos;
+    unsigned int count = size;
+    int ret = 0;
+    struct global_mem_dev *dev = filp->private_data;
+
+    if (p >= GLOBAL_MEM_SIZE)
+        return 0;
+
+    if (count > GLOBAL_MEM_SIZE - p)
+        count = GLOBAL_MEM_SIZE - p;
+
+    if (copy_to_user(buf, (void *)(dev->mem + p), count)) {
+        ret = -EFAULT;
+    } else {
+        *ppos += count;
+        ret = count;
+        printk(KERN_INFO "read %u bytes(s) from %lu\n", count, p);
+    }
+
+    return ret;
+}
+
+static ssize_t global_mem_write(struct file *filp, const char __user *buf, size_t size, loff_t *ppos)
+{
+    unsigned long p =  *ppos;
+    unsigned int count = size;
+    int ret = 0;
+    struct global_mem_dev *dev = filp->private_data;
+
+    if (p >= GLOBAL_MEM_SIZE)
+        return 0;
+
+    if (count > GLOBAL_MEM_SIZE - p)
+        count = GLOBAL_MEM_SIZE - p;
+
+    if (copy_from_user(dev->mem + p, buf, count)) {
+        ret = -EFAULT;
+    } else {
+        *ppos += count;
+        ret = count;
+        printk(KERN_INFO "written %u bytes(s) from %lu\n", count, p);
+    }
+
+    return ret;
+}
+
 static const struct file_operations global_mem_fops = {
     .owner = THIS_MODULE,
+    .open = global_mem_open,
+    .read = global_mem_read,
+    .write = global_mem_write,
+    .release = global_mem_release,
 };
 
 static int __init global_mem_init(void)
